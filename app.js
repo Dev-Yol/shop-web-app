129dd811cec449c87fc86c0904cddbcd2d3942a2
leordancarmona07 <leordan.carmona@student.passerellesnumeriques.org>
2020-11-19 20:15:25 +0800

    adding auth

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ const expressLayout = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
+// Static Files
+app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
+// Set Templating Engine
 app.set('view engine', 'ejs')
 app.set('layout', 'layouts/app')
 app.use(expressLayout)
 
-app.use(express.static('public'))
+// Routes
+app.get('', (req, res) => {
+    res.render('index',{
+        title: "Home page"
+    })
+})
 
 
 
 
-app.listen(port)
+app.listen(port, console.log(`Listening to port ${port}!`))
