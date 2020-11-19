717acee15385907726fd150b3ecfb6b523bb3b98
=Coca <=girlycoca143@gmail.com>
2020-11-19 15:19:27 +0800

    install setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const expressLayout = require('express-ejs-layouts')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.set('view engine', 'ejs')
+app.set('layout', 'layouts/app')
+app.use(expressLayout)
+
+app.use(express.static('public'))
+
+
+
+
+app.listen(port)
