5ec9cd6ccb6e4082a902684170a12b9391a21d4e
=Coca <=girlycoca143@gmail.com>
2020-11-24 21:05:17 +0800

    11/24/2020

--- app.js
+++ app.js
@@ -31,7 +31,55 @@ app.get("/auth/welcome", function(req, res) {
 
 app.get('/', (req, res) => {
     res.render('index', {
-        title: "Shoe Shop"
+        title: "Shoe Shop",
+        data: [{
+                id: 1,
+                name: "Waterproof walking shoes for men",
+                image: "/image/image8.jpg",
+                description: ""
+            },
+            {
+                id: 2,
+                name: "Everlane's First Sneaker",
+                image: "/image/image9.png",
+                description: ""
+            },
+            {
+                id: 3,
+                name: "Medical Work Shoe for Men",
+                image: "/image/image10.jpg",
+                description: ""
+            },
+            {
+                id: 4,
+                name: "Adidas Yung-1",
+                image: "/image/image11.jpg",
+                description: ""
+            },
+            {
+                id: 5,
+                name: "White Sneaker",
+                image: "/image/image14.jpg"
+            },
+            {
+                id: 6,
+                name: "Adidas Running Shoes",
+                image: "/image/image13.jpg",
+                description: ""
+            },
+            {
+                id: 7,
+                name: "Swedish Men's Shoes",
+                image: "/image/image15.jpg",
+                description: ""
+            },
+            {
+                id: 8,
+                name: "Elten Smart Shoes",
+                image: "/image/image16.jpg",
+                description: ""
+            },
+        ]
     })
 })
 app.get('*', (req, res) => {
