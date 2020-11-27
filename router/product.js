023d9665ee58c17503effbc6d6ec918edb1d0c0f
leordancarmona07 <leordan.carmona@student.passerellesnumeriques.org>
2020-11-27 17:24:35 +0800

    committing admin changes

--- router/product.js
+++ router/product.js
@@ -1,13 +1,23 @@
 const upload = require('../services/ImageUpload')
 const express = require("express");
+const { Product } = require('../models');
 const routes = express.Router();
 
-routes.route("/upload").post(upload.single('img'), (req, res) => {
-    let details = JSON.parse(req.body.details)
-    let imageName = '';
+routes.route("/add").post(upload.single('image'), (req, res) => {
+    let image = '';
     if (req.file) {
-        imageName = req.file.filename
+        image = req.file.filename
     }
+let details = JSON.parse(req.body.details);
+details.image = image;
+
+let product = new Product(details);
+    product.save((err, product)=>{
+        if (err) {
+            res.status(500).send({error: err})
+        }
+         res.send({message:'new product' ,  product })
+    })
 });
 
 
